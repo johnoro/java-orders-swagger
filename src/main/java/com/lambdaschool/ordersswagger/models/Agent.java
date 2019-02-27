08260cc13559f1ffacc9b615fc874408142968e9
johnoro <officialjohnorourke@gmail.com>
2019-02-27 17:05:58 -0500

    Adds models

---
+++ src/main/java/com/lambdaschool/ordersswagger/models/Agent.java
@@ -0,0 +1,95 @@
+package com.lambdaschool.ordersswagger.models;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "agents")
+public class Agent {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "agentcode", nullable = false)
+  private Long id;
+
+  @Column(name = "agentname")
+  private String name;
+
+  @Column(name = "workingarea")
+  private String workingArea;
+
+  private double commission;
+  private String phone;
+  private String country;
+
+  @OneToMany(cascade = CascadeType.REMOVE, mappedBy = "agent")
+  private Set<Customer> customers;
+
+  @OneToMany(cascade = CascadeType.REMOVE, mappedBy = "agent")
+  private Set<Order> orders;
+
+  public Agent() {}
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getWorkingArea() {
+    return workingArea;
+  }
+
+  public void setWorkingArea(String workingArea) {
+    this.workingArea = workingArea;
+  }
+
+  public double getCommission() {
+    return commission;
+  }
+
+  public void setCommission(double commission) {
+    this.commission = commission;
+  }
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
+
+  public String getCountry() {
+    return country;
+  }
+
+  public void setCountry(String country) {
+    this.country = country;
+  }
+
+  public Set<Customer> getCustomers() {
+    return customers;
+  }
+
+  public void setCustomers(Set<Customer> customers) {
+    this.customers = customers;
+  }
+
+  public Set<Order> getOrders() {
+    return orders;
+  }
+
+  public void setOrders(Set<Order> orders) {
+    this.orders = orders;
+  }
+}
---
+++ src/main/java/com/lambdaschool/ordersswagger/models/Customer.java
@@ -0,0 +1,156 @@
+package com.lambdaschool.ordersswagger.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "customers")
+public class Customer {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "custcode", nullable = false)
+  private Long id;
+
+  @Column(name = "custname", nullable = false)
+  private String name;
+
+  @Column(name = "custcity")
+  private String city;
+
+  @Column(name = "workingarea")
+  private String workingArea;
+
+  @Column(name = "custcountry")
+  private String country;
+
+  @Column(name = "openingamt")
+  private double openingAmount;
+
+  @Column(name = "receiveamt")
+  private double receiveAmount;
+
+  @Column(name = "paymentamt")
+  private double paymentAmount;
+
+  @Column(name = "outstandingamt")
+  private double outstandingAmount;
+
+  private String grade;
+  private String phone;
+
+  @ManyToOne
+  @JoinColumn(name = "agentcode", nullable = false)
+  @JsonIgnore
+  private Agent agent;
+
+  @OneToMany(cascade = CascadeType.REMOVE, mappedBy = "customer")
+  private Set<Order> orders;
+
+  public Customer() {}
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getCity() {
+    return city;
+  }
+
+  public void setCity(String city) {
+    this.city = city;
+  }
+
+  public String getWorkingArea() {
+    return workingArea;
+  }
+
+  public void setWorkingArea(String workingArea) {
+    this.workingArea = workingArea;
+  }
+
+  public String getCountry() {
+    return country;
+  }
+
+  public void setCountry(String country) {
+    this.country = country;
+  }
+
+  public double getOpeningAmount() {
+    return openingAmount;
+  }
+
+  public void setOpeningAmount(double openingAmount) {
+    this.openingAmount = openingAmount;
+  }
+
+  public double getReceiveAmount() {
+    return receiveAmount;
+  }
+
+  public void setReceiveAmount(double receiveAmount) {
+    this.receiveAmount = receiveAmount;
+  }
+
+  public double getPaymentAmount() {
+    return paymentAmount;
+  }
+
+  public void setPaymentAmount(double paymentAmount) {
+    this.paymentAmount = paymentAmount;
+  }
+
+  public double getOutstandingAmount() {
+    return outstandingAmount;
+  }
+
+  public void setOutstandingAmount(double outstandingAmount) {
+    this.outstandingAmount = outstandingAmount;
+  }
+
+  public String getGrade() {
+    return grade;
+  }
+
+  public void setGrade(String grade) {
+    this.grade = grade;
+  }
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
+
+  public Agent getAgent() {
+    return agent;
+  }
+
+  public void setAgent(Agent agent) {
+    this.agent = agent;
+  }
+
+  public Set<Order> getOrders() {
+    return orders;
+  }
+
+  public void setOrders(Set<Order> orders) {
+    this.orders = orders;
+  }
+}
