d4fe1c80dfbc2dba1fa74b378a1200908b4b8cef
johnoro <officialjohnorourke@gmail.com>
2019-02-27 16:52:10 -0500

    Bootstraps project

---
+++ src/test/java/com/lambdaschool/ordersswagger/OrdersswaggerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lambdaschool.ordersswagger;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OrdersswaggerApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
