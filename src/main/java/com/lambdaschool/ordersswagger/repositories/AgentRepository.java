273422cc61e0c8d40a93c3a4b90c523ee4b48c16
johnoro <officialjohnorourke@gmail.com>
2019-02-27 17:07:30 -0500

    Adds repositories

---
+++ src/main/java/com/lambdaschool/ordersswagger/repositories/AgentRepository.java
@@ -0,0 +1,5 @@
+package com.lambdaschool.ordersswagger.repositories;
+
+import com.lambdaschool.ordersswagger.models.Agent;
+
+public interface AgentRepository extends Repository<Agent> {}
---
+++ src/main/java/com/lambdaschool/ordersswagger/repositories/OrderRepository.java
@@ -0,0 +1,5 @@
+package com.lambdaschool.ordersswagger.repositories;
+
+import com.lambdaschool.ordersswagger.models.Order;
+
+public interface OrderRepository extends Repository<Order> {}
